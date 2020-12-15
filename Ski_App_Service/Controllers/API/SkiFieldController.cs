7fcf495c8d90fcb364b6164b9bc44e884dd67d09
Jordan Elley <jordanelley@hotmail.com>
2020-12-16 00:37:19 +1300

    first commit

---
+++ Ski_App_Service/Controllers/API/SkiFieldController.cs
@@ -0,0 +1,17 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+namespace Ski_App_Service.Controllers.API
+{
+    [Route("addSkiField")] 
+    [ApiController]
+
+    public class SkiFieldController
+    {
+        [HttpPost]
+        public void AddNewSkiFieldReview()
+        {
+            
+        }
+    }
+}
