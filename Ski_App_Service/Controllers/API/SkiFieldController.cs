1c03375283a99d632a2fd90f9896b17899119095
Jordan Elley <jordanelley@hotmail.com>
2020-12-16 23:23:09 +1300

    added some basic endpoints

--- Ski_App_Service/Controllers/API/SkiFieldController.cs
+++ Ski_App_Service/Controllers/API/SkiFieldController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -8,10 +9,18 @@ namespace Ski_App_Service.Controllers.API
 
     public class SkiFieldController
     {
+        private readonly List<string> _skiFieldReviews = new List<string>();
+
+
         [HttpPost]
-        public void AddNewSkiFieldReview()
+        public void AddNewSkiFieldReview(string review)
         {
-            
+            _skiFieldReviews.Add(review);
+        }
+        [HttpGet]
+        public List<string> GetSkiFieldReviews(string review)
+        {
+            return _skiFieldReviews;
         }
     }
 }
